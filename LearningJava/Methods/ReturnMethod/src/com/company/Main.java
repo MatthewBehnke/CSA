8694a3499fea3f259fcc41f327ad4e1dadb2da04
Matt Behnke <matthewbehnke0@gmail.com>
2019-01-16 14:31:07 -0600

    added a return method

---
+++ LearningJava/Methods/ReturnMethod/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    System.out.println(squareAVar(5));
+    }
+
+    public static int squareAVar(int n) {
+        return n * n;
+    }
+}
