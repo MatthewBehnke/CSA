8fb8fe14d3d26ce65778ae35f84392cdea05c083
Matt Behnke <matthewbehnke0@gmail.com>
2019-01-16 14:24:52 -0600

    added method with an input

---
+++ LearningJava/Methods/MethodWithInput/src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    printArg("Some String");
+    }
+
+    public static void printArg(String myString) {
+        System.out.println(myString);
+    }
+
+
+}
