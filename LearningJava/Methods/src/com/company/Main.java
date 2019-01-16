6d7b0980a3dedb39e126c2c381682ab0ac0a1c09
Matt Behnke <matthewbehnke0@gmail.com>
2019-01-16 14:16:15 -0600

    added Methods example

---
+++ LearningJava/Methods/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    printToConsole();
+    }
+
+    public static void printToConsole() {
+        System.out.println("Some Text");
+    }
+}
